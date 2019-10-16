da81156df0760188466cac94b2bf35af845f997c
Chiu97 <33931728+Chiu97@users.noreply.github.com>
2019-10-17 00:22:09 +0800

    10-16

--- Design-Pattern/Structral/adapter-pattern.js
+++ Design-Pattern/Structral/adapter-pattern.js
@@ -27,16 +27,19 @@ class NewCalculator {
 // new interface
 class CalAdapter {
   constructor() {
-    const newCalc = new NewCalculator();
+    this.newCalc = new NewCalculator();
   }
   operations(item1, item2, operator) {
     switch(operator) {
       case 'add':
-        return newCalc.add(item1,item2);
+        return this.newCalc.add(item1,item2);
       case 'sub':
-        return newCalc.sub(item1,item2);
+        return this.newCalc.sub(item1,item2);
       default:
         return NaN;
     }
   }
 }
+
+let adapter = new CalAdapter();
+adapter.operations(4,3,'add') // 7
