0b0637639a83671458bf33788b51e267a0e21511
chiu97 <846262576@qq.com>
2020-12-24 21:18:25 +0800

    daily: flatten

--- Common/Flatten/index.js
+++ Common/Flatten/index.js
@@ -1,7 +1,22 @@
-const flatten = function(arr) {
-  if(Array.isArray(arr)) {
-    return arr;
-  } else {
-    arr.forE
-  }
+const _ = require('lodash')
+const assert = require('assert')
+const baseFlatten = (val, depth) => {
+  if (!Array.isArray(val)||!depth) return val
+  let res = []
+
+  val.forEach(item => {
+    if (!Array.isArray(item)) {
+      res.push(item)
+      return
+    }
+
+    res.push(...baseFlatten(item, depth-1))
+  })
+
+  return res
 }
+
+const testCase = [1, [2, [3, 4, [5, 6]], [7]]]
+const testRes = baseFlatten(testCase, 2)
+const expectedOutput = _.flattenDepth(testCase, 2)
+assert.deepEqual(testRes, expectedOutput, 'Base Flatten should behave just like lodash flatten')
