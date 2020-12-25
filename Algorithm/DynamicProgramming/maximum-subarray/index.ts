edba1aa34eca7bc9142add2a775bda7351febca4
chiu97 <846262576@qq.com>
2020-12-25 10:36:07 +0800

    leetcode:

---
+++ Algorithm/DynamicProgramming/maximum-subarray/index.ts
@@ -0,0 +1,20 @@
+function maxSubArray(nums: number[]): number {
+    if (nums.length===0) return 0
+    
+    let preSum = 0
+    let max = nums[0]
+
+    for (let i=1; i<nums.length; i++) {
+        const val = nums[i]
+        let curSum = preSum + val
+        if (preSum+val>0) preSum = preSum+val
+        else preSum = 0
+        if (curSum>max) max = curSum
+    }
+
+    return max
+};
+
+const testCase = [-2,1,-3,4,-1,2,1,-5,4]
+const output = maxSubArray(testCase)
+console.log(output)
