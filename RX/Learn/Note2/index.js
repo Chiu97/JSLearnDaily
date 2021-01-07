4fce40c340ff50b9d8e14e794a607f6d1fc7bd5d
chiu97 <846262576@qq.com>
2021-01-07 16:25:04 +0800

    daily: rx take

--- RX/Learn/Note2/index.js
+++ RX/Learn/Note2/index.js
@@ -1,13 +1,14 @@
 "use strict";
 exports.__esModule = true;
 var rxjs_1 = require("rxjs");
+var operators_1 = require("rxjs/operators");
 function subscribeToTheRXButton() {
     var btn = document.querySelector('#rx-source-button');
     var counter = 0;
     if (!btn)
         return;
     var source$buttonClick = rxjs_1.fromEvent(btn, 'click');
-    var subscription$buttonClick = source$buttonClick.subscribe(function (event) {
+    var subscription$buttonClick = source$buttonClick.pipe(operators_1.take(3)).subscribe(function (event) {
         console.log(event);
         if (++counter > 3)
             subscription$buttonClick.unsubscribe();
--- RX/Learn/Note2/index.ts
+++ RX/Learn/Note2/index.ts
@@ -1,4 +1,5 @@
 import { fromEvent } from "rxjs"
+import { take } from "rxjs/operators"
 
 function subscribeToTheRXButton () {
     const btn: HTMLButtonElement = document.querySelector('#rx-source-button')
@@ -6,7 +7,7 @@ function subscribeToTheRXButton () {
     if (!btn) return
 
     const source$buttonClick = fromEvent(btn, 'click') 
-    const subscription$buttonClick = source$buttonClick.subscribe(event => {
+    const subscription$buttonClick = source$buttonClick.pipe(take(3)).subscribe(event => {
         console.log(event)
         if (++counter>3) subscription$buttonClick.unsubscribe()
     })
