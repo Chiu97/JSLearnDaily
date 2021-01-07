47f813eda7a58cd3f0371135e19e288984a73c49
chiu97 <846262576@qq.com>
2021-01-07 18:19:52 +0800

    rx: scan

--- RX/Learn/Note3/index.ts
+++ RX/Learn/Note3/index.ts
@@ -1,12 +1,12 @@
-import { filter, map, take } from 'rxjs/operators'
+import { filter, map, scan, take } from 'rxjs/operators'
 import { combineLatest, interval, of } from 'rxjs'
 
+const logOps = (v: any) => console.log(v)
+const println = () => console.log('-'.repeat(60))
 export function TestPipe() {
     const source_ofNum = of(1, -3, 4, 5, 7, -2)
     const doubleOps = (num: number) => num*2
     const positiveFilter = (num: number) => num>0
-    const logOps = (v: any) => console.log(v)
-    const println = () => console.log('-'.repeat(60))
     const source_positiveNums = source_ofNum.pipe(filter(positiveFilter))
     const source_doubleNums = source_ofNum.pipe(map(doubleOps))
     const source_positiveDoubleNums = source_ofNum.pipe(filter(positiveFilter), map(doubleOps))
@@ -25,5 +25,12 @@ export function TestCombine() {
     source_combine.subscribe(v => console.log(v))
 }
 
-TestPipe()
-TestCombine()
+export function TestScan() {
+    const source_number = interval(20).pipe(take(5))
+    const source_counter = source_number.pipe(scan((acc, _) => acc+1, 0))
+    source_counter.forEach(logOps)
+}
+
+// TestPipe()
+// TestCombine()
+TestScan()
